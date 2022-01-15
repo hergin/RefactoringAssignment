a65886169f621c1cdea7350352877a5f2b28e60a
Durbin, Hunter <hunterdurbin000@gmail.com>
2022-01-15 12:13:59 -0500

    BONUS
    Signed-off-by: Hunter Durbin <hmdurbin@bsu.edu>

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,20 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class InsuranceCalculator {
 
     private InsuranceStrategy strategy;
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
+        Map<InsuranceType, Rule<Double>> rules = createRules(income);
+        return Stream.of(InsuranceType.values())
+                .filter(insuranceType -> rules.get(insuranceType).condition.get())
+                .map(insuranceType -> rules.get(insuranceType).process.get())
+                .findFirst()
+                .orElse(-1.0);
+    }
+
+    Map<InsuranceType, Rule<Double>> createRules(double income) {
+        Map<InsuranceType, Rule<Double>> createRules = new HashMap<>();
+        createRules.put(InsuranceType.LOW, createRuleForLowInsurance(income));
+        createRules.put(InsuranceType.MEDIUM, createRuleForMediumInsurance(income));
+        createRules.put(InsuranceType.HIGH, createRuleForHighInsurance(income));
+        createRules.put(InsuranceType.VERY_HIGH, createRuleForVeryHighInsurance(income));
+
+        return createRules;
+    }
+
+    Rule<Double> createRuleForVeryHighInsurance(double income) {
+        return createRule(
+                ()->(true),
+                ()-> new InsuranceStrategyVeryHigh().calculate(income)
+        );
+    }
+
+    Rule<Double> createRuleForHighInsurance(double income) {
+        return createRule(
+                ()->(income <= 60000),
+                ()-> new InsuranceStrategyHigh().calculate(income)
+        );
+    }
+
+    Rule<Double> createRuleForMediumInsurance(double income) {
+        return createRule(
+                ()->(income <= 30000),
+                ()-> new InsuranceStrategyMedium().calculate(income)
+        );
+    }
+
+    Rule<Double> createRuleForLowInsurance(double income) {
+        return createRule(
+                ()->(income <= 10000),
+                ()-> new InsuranceStrategyLow().calculate(income)
+        );
+    }
+
+    Rule<Double> createRule(Supplier<Boolean> condition, Supplier<Double> process) {
+        return new Rule<>(condition, process);
     }
 }
