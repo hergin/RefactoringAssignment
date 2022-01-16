aa1977e93c46ffe4e215d6ec96fb997426222d5e
spencernaugler7 <spencernaugler7@gmail.com>
2022-01-16 10:54:54 -0500

    implement abstract class

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,31 +1,20 @@
 public class InsuranceCalculator {
 
+    private InsuranceStrategy strategy;
+
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            strategy = new InsuranceStrategyLow();
+            return strategy.calculate(income);
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            strategy = new InsuranceStrategyMedium();
+            return strategy.calculate(income);
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            strategy = new InsuranceStrategyHigh();
+            return strategy.calculate(income);
         } else {
-            return calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculate(income);
         }
     }
-
-    private double calculateInsuranceVeryHigh(double income) {
-        return (income - getAdjustment()) * getWeight() + getConstant();
-    }
-
-    private int getConstant() {
-        return 105600;
-    }
-
-    private double getWeight() {
-        return 0.02;
-    }
-
-    private int getAdjustment() {
-        return 60000;
-    }
-
 }
