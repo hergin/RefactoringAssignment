809b7208010dc2646b3ca38a597cd28a9deae021
dotty <dottyhorsman@yahoo.com>
2022-01-18 12:07:48 -0500

    completed "Using Extract Delegate refactoring" step

---
+++ src/InsuranceStrategyVeryHigh.java
@@ -0,0 +1,20 @@
+public class InsuranceStrategyVeryHigh {
+    public InsuranceStrategyVeryHigh() {
+    }
+
+    public double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public int getAdjustment() {
+        return 60000;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+}
