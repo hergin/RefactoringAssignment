50c35dd707abea91f4c58d67531ad7385d7a871a
dotty <dottyhorsman@yahoo.com>
2022-01-18 12:25:59 -0500

    completed "Fine tuning" step + added ifs package

--- src/ifs/InsuranceStrategyVeryHigh.java
+++ src/ifs/InsuranceStrategyVeryHigh.java
@@ -1,19 +1,19 @@
-public class InsuranceStrategyVeryHigh {
+package ifs;
+public class InsuranceStrategyVeryHigh extends InsuranceStrategy {
     public InsuranceStrategyVeryHigh() {
     }
 
-    public double calculateInsuranceVeryHigh(double income) {
-        return (income - getAdjustment()) * getWeight() + getConstant();
-    }
-
+    @Override
     public int getAdjustment() {
         return 60000;
     }
 
+    @Override
     public double getWeight() {
         return 0.02;
     }
 
+    @Override
     public int getConstant() {
         return 105600;
     }
--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -1,3 +1,5 @@
+
+import ifs.InsuranceCalculator;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
