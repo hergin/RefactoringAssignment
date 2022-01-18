60a518555e3eb33c553cd91e575d7022514b5d62
dotty <dottyhorsman@yahoo.com>
2022-01-18 12:53:44 -0500

    completed "Implementing the abstract class" and "Happy end" steps

--- src/ifs/InsuranceStrategy.java
+++ src/ifs/InsuranceStrategy.java
@@ -1,6 +1,6 @@
 package ifs;
 public abstract class InsuranceStrategy {
-    public double calculateInsuranceVeryHigh(double income) {
+    public double calculate(double income) {
         return (income - getAdjustment()) * getWeight() + getConstant();
     }
 
---
+++ src/ifs/InsuranceStrategyLow.java
@@ -0,0 +1,16 @@
+package ifs;
+
+public class InsuranceStrategyLow extends InsuranceStrategy {
+    @Override
+    public int getAdjustment() { return 0; }
+
+    @Override
+    public double getWeight() {
+        return 0.365;
+    }
+
+    @Override
+    public int getConstant() {
+        return 0;
+    }
+}
