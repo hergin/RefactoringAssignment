07e962428d24dbc82cae55c3d5a619e5af05750d
423rd <kmmatchett@bsu.edu>
2022-01-21 19:39:43 -0500

    Using Extract Delegate refactoring

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,30 +1,16 @@
 public class InsuranceCalculator {
 
+    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            return income * 0.365;
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            return (income - 10000) * 0.2 + 35600;
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return (income - 30000) * 0.1 + 76500;
         } else {
-            return getaDouble(income);
+            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
         }
-    }
-
-    private double getaDouble(double income) {
-        return (income- getAdjustment())* getWeight() + getConstant();
-    }
-
-    private int getAdjustment() {
-        return 60000;
-    }
-
-    private double getWeight() {
-        return 0.02;
-    }
-
-    private int getConstant() {
-        return 105600;
     }
 }
---
+++ src/InsuranceStrategyVeryHigh.java
@@ -0,0 +1,20 @@
+public class InsuranceStrategyVeryHigh {
+    public InsuranceStrategyVeryHigh() {
+    }
+
+    public double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+
+    public int getAdjustment() {
+        return 60000;
+    }
+}
