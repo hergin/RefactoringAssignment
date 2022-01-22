e889a059f2202f91349c0adc7163953d8cbb5d71
Annie Peabody <aepeabody@bsu.edu>
2022-01-21 19:41:12 -0500

    refactor insurance calculator

---
+++ src/InsuranceStrategyMedium.java
@@ -0,0 +1,18 @@
+public class InsuranceStrategyMedium extends InsuranceStrategy {
+    public InsuranceStrategyMedium(){}
+
+    @Override
+    int getConstant() {
+        return 35600;
+    }
+
+    @Override
+    double getWeight() {
+        return 0.2;
+    }
+
+    @Override
+    int getAdjustment() {
+        return 10000;
+    }
+}
