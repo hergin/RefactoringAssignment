481dc77ad6836b58d647daf6a27450a05f3cb1fb
Jacob Bishop <jacobbishop1000@gmail.com>
2023-01-17 00:54:08 -0500

    Using Extract Delegate refactoring

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,30 +1,14 @@
 public class InsuranceCalculator {
-
+    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            return income * 0.365;
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            return (income - 10000) * 0.2 + 35600;
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return (income - 30000) * 0.1 + 76500;
         } else {
-            return calculateInsuranceVeryHigh(income);
+            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
         }
-    }
-
-    private static double calculateInsuranceVeryHigh(double income) {
-        return (income - getConstant()) * getWeight() + getAdjustment();
-    }
-
-    private static int getAdjustment() {
-        return 105600;
-    }
-
-    private static double getWeight() {
-        return 0.02;
-    }
-
-    private static int getConstant() {
-        return 60000;
     }
 }
