04fef3cd4b67d2fca9799ed488b8bad87998524b
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2023-01-20 14:26:09 -0500

    Fine Tuning

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,7 +1,6 @@
 public class InsuranceCalculator {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
-
+    private InsuranceStrategyVeryHigh strategy;
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             return income*0.365;
@@ -10,24 +9,9 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
-    }
-
-    private double calculateInsuranceVeryHigh(double income) {
-        return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
-    }
-
-    private int getConstant() {
-        return insuranceStrategyVeryHigh.getConstant();
-    }
-
-    private double getWeight() {
-        return insuranceStrategyVeryHigh.getWeight();
-    }
-
-    private int getAdjustment() {
-        return insuranceStrategyVeryHigh.getAdjustment();
     }
 
 }
