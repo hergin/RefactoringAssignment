e520aad86f22dd3782bf8526f5ca34e9c4490869
ejperezz <ejperez@bsu.edu>
2023-01-22 15:30:13 -0500

    Implementing the abstract class

---
+++ src/InsuranceStrategyLow.java
@@ -0,0 +1,15 @@
+public class InsuranceStrategyLow extends InsuranceStrategy {
+    @Override
+    int getConstant() {
+        return 0;
+    }
+    @Override
+    double getWeight() {
+        return 0.365;
+    }
+    @Override
+    int getAdjustment() {
+        return 0;
+    }
+}
+
---
+++ src/InsuranceStrategyMedium.java
@@ -0,0 +1,18 @@
+public class InsuranceStrategyMedium extends InsuranceStrategy {
+    @Override
+    int getConstant() {
+        return 35600;
+    }
+
+    @Override
+    double getWeight() {
+        return 0.2;
+    }
+
+    @Override
+    int getAdjustment() {
+        return 10000;
+    }
+}
+
+
