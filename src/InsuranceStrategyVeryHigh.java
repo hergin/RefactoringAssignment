8c1d144e861adfbcc0eec85af9fa85b2f094e23d
Nate Slagter <nateslagter@gmail.com>
2023-01-22 14:19:17 -0500

    Using Extract Delegate refactoring

---
+++ src/InsuranceStrategyVeryHigh.java
@@ -0,0 +1,20 @@
+public class InsuranceStrategyVeryHigh {
+    public InsuranceStrategyVeryHigh() {
+    }
+
+    double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    int getAdjustment() {
+        return 60000;
+    }
+
+    double getWeight() {
+        return 0.02;
+    }
+
+    int getConstant() {
+        return 105600;
+    }
+}
