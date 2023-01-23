177c5fde8df9ef5e7e3ffc660e272272a4c2b931
Wesley <wjkring@bsu.edu>
2023-01-22 23:15:25 -0500

    Extra Credit

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -3,19 +3,22 @@ public class InsuranceCalculator {
     private static InsuranceStrategy strategy;
 
     public static double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
+        switch ((int) income / 10000) {
+            case 0:
+            case 1:
+                strategy = new InsuranceStrategyLow();
+                return strategy.calculate(income);
+            case 2:
+            case 3:
+                strategy = new InsuranceStrategyMedium();
+                return strategy.calculate(income);
+            case 4:
+            case 5:
+                strategy = new InsuranceStrategyHigh();
+                return strategy.calculate(income);
+            default:
+                strategy = new InsuranceStrategyVeryHigh();
+                return strategy.calculate(income);
         }
     }
-
 }
