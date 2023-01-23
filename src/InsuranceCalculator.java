07e5adcb7a95324c9fd453b74d3713d02e2ab4d9
bringobrongo <jtbrown2@bsu.edu>
2023-01-22 20:49:37 -0500

    Fine tuning

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,4 +1,6 @@
-public class InsuranceCalculator {
+class InsuranceCalculator {
+
+    private InsuranceStrategy strategy;
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
@@ -8,8 +10,10 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return InsuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
+
     }
 
 }
--- src/InsuranceStrategyVeryHigh.java
+++ src/InsuranceStrategyVeryHigh.java
@@ -1,17 +1,16 @@
-public class InsuranceStrategyVeryHigh {
-    static double calculateInsuranceVeryHigh(double income) {
-        return (income - getAdjustment()) * getWeight() + getConstant();
-    }
-
-    static int getConstant() {
+public class InsuranceStrategyVeryHigh extends InsuranceStrategy {
+    @Override
+    public int getConstant() {
         return 105600;
     }
 
-    static double getWeight() {
+    @Override
+    public double getWeight() {
         return 0.02;
     }
 
-    static int getAdjustment() {
+    @Override
+    public int getAdjustment() {
         return 60000;
     }
 }
