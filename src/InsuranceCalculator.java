e0d624e33186f2688ba9282f7218cc580abd5a8d
alexh <arhupe@bsu.edu>
2023-01-23 02:04:40 -0500

    Extracting Methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -8,8 +8,25 @@ public class InsuranceCalculator{
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return (income-60000)*0.02+105600;
+            return calculateInsuranceVeryHigh(income);
         }
     }
+
+    private double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    private static int getAdjustment() {
+        return 60000;
+    }
+
+    private static double getWeight() {
+        return 0.02;
+    }
+
+    private static int getConstant() {
+        return 105600;
+    }
+
 
 }
