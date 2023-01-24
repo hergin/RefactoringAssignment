cc2243ebce2e10f0f13c78b3906c5f5489364482
Kiri Woodruff <kdwoodruff@bsu.edu>
2023-01-24 12:16:39 -0500

    Fine tuning

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,9 +1,8 @@
 public class InsuranceCalculator {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+    private InsuranceStrategy strategy;
 
     public double calculateInsurance(double income) {
-
 
         if (income <= 10000) {
             return income*0.365;
@@ -12,7 +11,8 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
     }
 }
