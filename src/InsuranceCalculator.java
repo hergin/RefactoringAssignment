b10a272d53146909aeb8d36cf74264d59db31440
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2024-01-11 14:07:08 -0500

    Remove all if logic

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,20 +1,30 @@
+import static java.lang.Math.*;
+
 public class InsuranceCalculator {
 
-    private InsuranceStrategy strategy;
+    private final InsuranceStrategy[] strategies;
+
+    public InsuranceCalculator() {
+        strategies = new InsuranceStrategy[4];
+        strategies[0] = new InsuranceStrategyLow();
+        strategies[1] = new InsuranceStrategyMedium();
+        strategies[2] = new InsuranceStrategyHigh();
+        strategies[3] = new InsuranceStrategyVeryHigh();
+    }
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
+        return decideStrategy(income).calculate(income);
+    }
+
+    private InsuranceStrategy decideStrategy(double income) {
+        income = max(income, 0);
+        // There are other strategies for this, including signum, abs, etc, but this is probably the cleanest way
+        // to accomplish the mapping.
+        int index = (int)(
+                min(ceil(income/10_000)-1, 1)
+                + min(ceil(income/30_000)-1, 1)
+                + min(ceil(income/60_000)-1, 1)
+        );
+        return strategies[index];
     }
 }
