f76ed09411d5d1e89f42451137847a1f6792df14
Jack Carthew <jdcarthew@bsu.edu>
2024-01-12 09:09:26 -0800

    Extracting methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,15 +1,36 @@
 public class InsuranceCalculator {
 
-    public double calculateInsurance(double income) {
-        if (income <= 10000) {
+    public double calculateInsurance(double income)
+    {
+        if (income <= 10000)
+        {
             return income*0.365;
-        } else if (income <= 30000) {
+        } else if (income <= 30000)
+        {
             return (income-10000)*0.2+35600;
-        } else if (income <= 60000) {
+        } else if (income <= 60000)
+        {
             return (income-30000)*0.1+76500;
-        } else {
-            return (income-60000)*0.02+105600;
+        } else
+        {
+            return calculateInsuranceVeryHigh(income);
         }
+    }
+
+    private static double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    private static int getConstant() {
+        return 105600;
+    }
+
+    private static double getWeight() {
+        return 0.02;
+    }
+
+    private static int getAdjustment() {
+        return 60000;
     }
 
 }
