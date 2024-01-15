4f56772a112e736da6a4fe7de88e7940e56d1d05
Ethan Forrest <eforrest8@outlook.com>
2024-01-15 17:18:10 -0500

    Bonus attempt
    Does Math.min() count as a conditional structure?
    Does accessing an array by index count as a conditional structure?
    I'm sure I've done this wrong, but this is the best I got.

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,21 +1,10 @@
 public class InsuranceCalculator {
 
-    private InsuranceStrategy strategy;
+    private final InsuranceStructure structure = new InsuranceStructure();
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculateInsurance(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculateInsurance(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculateInsurance(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculateInsurance(income);
-        }
+        InsuranceStrategy strategy = structure.getStrategy(income);
+        return strategy.calculateInsurance(income);
     }
 
 }
