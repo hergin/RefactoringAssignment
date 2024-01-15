58010141d3f240c881f3b1e35d368252bcf455c2
Piesrgr8 <jaredbruiser@hotmail.com>
2024-01-15 03:09:19 -0500

    Happy End with No If-statements

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,20 +1,9 @@
 public class InsuranceCalculator {
 
-    private InsuranceStrategy strategy;
-
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
+        return (income <= 10000) ? new InsuranceStrategyLow().calculate(income)
+                : (income <= 30000) ? new InsuranceStrategyMedium().calculate(income)
+                : (income <= 60000) ? new InsuranceStrategyHigh().calculate(income)
+                : new InsuranceStrategyVeryHigh().calculate(income);
     }
 }
