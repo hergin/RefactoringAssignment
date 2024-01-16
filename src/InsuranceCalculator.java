7afee8118fbae0bc3f4ba1cc5ff50169d5ee8d79
Leah Casey <alcasey@bsu.edu>
2024-01-16 00:48:35 -0500

    Get rid of conditionals; delete prev method

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -15,25 +15,3 @@ public class InsuranceCalculator {
         return strategies.floorEntry(income).getValue().calculate(income);
     }
 }
-
-/*
-    private InsuranceStrategy strategy;
-
-    public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
-    }
-}
-
- */
