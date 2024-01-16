ad5b4dd68ad1edf72a5c23b8eddb73ebf06c8da6
Timothy Nicholas <tsnicholas@bsu.edu>
2024-01-16 12:54:58 -0500

    Replace if else structure.

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,20 +1,10 @@
 import ifs.*;
 
 public class InsuranceCalculator {
-    private InsuranceStrategy strategy;
+    private final InsuranceConditionFactory insuranceConditionFactory = new InsuranceConditionFactory();
+
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
+        InsuranceStrategy strategy = insuranceConditionFactory.getStrategy(income);
+        return strategy.calculate(income);
     }
 }
