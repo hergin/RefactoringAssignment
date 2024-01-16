9ab7cc5c80ec02342d0421e31a5d11e9621ef10c
Leah Casey <alcasey@bsu.edu>
2024-01-15 23:16:47 -0500

    Add NavigableMap to InsuranceCalculator.java

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,5 +1,22 @@
-public class InsuranceCalculator {
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
+public class InsuranceCalculator {
+    private final NavigableMap<Double, InsuranceStrategy> strategies = new TreeMap<>();
+
+    public InsuranceCalculator() {
+        strategies.put(0.0, new InsuranceStrategyLow());
+        strategies.put(10000.0, new InsuranceStrategyMedium());
+        strategies.put(30000.0, new InsuranceStrategyHigh());
+        strategies.put(60000.0, new InsuranceStrategyVeryHigh());
+    }
+
+    public double calculateInsurance(double income) {
+        return strategies.floorEntry(income).getValue().calculate(income);
+    }
+}
+
+/*
     private InsuranceStrategy strategy;
 
     public double calculateInsurance(double income) {
@@ -18,3 +35,5 @@ public class InsuranceCalculator {
         }
     }
 }
+
+ */
