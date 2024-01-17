12b970a41da13d324ce7288aecf0093dc9047b28
amecos <amecothecoop@gmail.com>
2024-01-17 18:10:30 -0500

    Fine tuning

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,6 +1,5 @@
 public class InsuranceCalculator {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
@@ -10,7 +9,8 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+            InsuranceStrategyVeryHigh strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
     }
 }
