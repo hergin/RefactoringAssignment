8bc8381c85a0b63e16dacc6e55974842ba0f238e
MichaelaWalker <112041868+MichaelaWalker@users.noreply.github.com>
2024-01-17 18:51:42 -0500

    Extracting methods

--- src/ifs/InsuranceCalculator.java
+++ src/ifs/InsuranceCalculator.java
@@ -10,8 +10,24 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return (income-60000)*0.02+105600;
+            return calculateInsuranceVeryHigh(income);
         }
+    }
+
+    private static double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    private static int getConstant() {
+        return 105600;
+    }
+
+    private static double getWeight() {
+        return 0.02;
+    }
+
+    private static int getAdjustment() {
+        return 60000;
     }
 
 }
