d6fca430fba6bb6305c75f29eed1f76ff03020a2
PandorasGit <wendallar125@gmail.com>
2024-01-17 17:07:07 -0500

    sing the Extract Delegate refactoring

---
+++ src/InsuranceStrategy.java
@@ -0,0 +1,13 @@
+public class InsuranceStrategy {
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getAdjustment() {
+        return 60000;
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+}
--- src/InsuranceStrategyVeryHigh.java
+++ src/InsuranceStrategyVeryHigh.java
@@ -1,17 +1,6 @@
-public class InsuranceStrategyVeryHigh {
-    static double calculateInsuranceVeryHigh(double income) {
+public class InsuranceStrategyVeryHigh extends InsuranceStrategy {
+    public double calculateInsuranceVeryHigh(double income) {
         return (income - getAdjustment()) * getWeight() + getConstant();
     }
 
-    static double getWeight() {
-        return 0.02;
-    }
-
-    static int getAdjustment() {
-        return 60000;
-    }
-
-    static int getConstant() {
-        return 105600;
-    }
 }
