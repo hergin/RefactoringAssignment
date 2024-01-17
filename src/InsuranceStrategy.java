68a5fc408d8e4bde4644ae4d543f6e9035107309
connorvessely <111907690+connorvessely@users.noreply.github.com>
2024-01-17 15:30:40 -0500

    Implementing the abstract class

--- src/InsuranceStrategy.java
+++ src/InsuranceStrategy.java
@@ -1,5 +1,5 @@
 public abstract class InsuranceStrategy {
-    public double calculateVeryHighInsurance(double income) {
+    public double calculate(double income) {
         return (income - getAdjustment()) * getWeight() + getConstant();
     }
 
