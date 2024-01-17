a3ae42bf19a368d459441fc0ad5362829ebac67a
joeygarberick <112030126+joeygarberick@users.noreply.github.com>
2024-01-17 10:36:24 -0500

    bonus

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,19 +1,19 @@
 public class InsuranceCalculator {
 
-    private InsuranceStrategy strategy;
+    final InsuranceStrategy[] strategies = {
+            new InsuranceStrategyLow(),
+            new InsuranceStrategyMedium(),
+            new InsuranceStrategyHigh(),
+            new InsuranceStrategyVeryHigh()
+    };
+
+    int boolToInt(boolean a) {
+        return Boolean.compare(a, false);
+    }
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-
-        }
-        return strategy.calculateInsurance(income);
+        int i = boolToInt(income>10000) + boolToInt(income>30000) + boolToInt(income>60000);
+        return strategies[i].calculateInsurance(income);
     }
 
 }
