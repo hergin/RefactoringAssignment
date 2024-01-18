40cbe13bcadde33c311b0776799e0bf9513ed565
Samuel Gross <52175733+Haitifan03@users.noreply.github.com>
2024-01-17 22:01:08 -0500

    Extracting Methods

--- test/ifs/InsuranceCalculator.java
+++ test/ifs/InsuranceCalculator.java
@@ -2,15 +2,17 @@ package ifs;
 
 public class InsuranceCalculator {
 
+    private InsuranceStrategyVeryHigh strategy;
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             return income*0.365;
         } else if (income <= 30000) {
             return (income-10000)*0.2+35600;
-        } else if (income <= 60000) {
+        } else if (income <= InsuranceStrategyVeryHigh.getAdjustment()) {
             return (income-30000)*0.1+76500;
         } else {
-            return (income-60000)*0.02+105600;
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
     }
 
