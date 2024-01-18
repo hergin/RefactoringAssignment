c12e4df89759225f9be27557bf93897fe2a852b0
MichaelaWalker <112041868+MichaelaWalker@users.noreply.github.com>
2024-01-17 19:03:23 -0500

    Using extract delegate refactoring

---
+++ src/ifs/InsuranceStrategyVeryHigh.java
@@ -0,0 +1,21 @@
+package ifs;
+
+public class InsuranceStrategyVeryHigh {
+    public InsuranceStrategyVeryHigh() {
+    }
+    public static double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    static int getConstant() {
+        return 105600;
+    }
+
+    static double getWeight() {
+        return 0.02;
+    }
+
+    static int getAdjustment() {
+        return 60000;
+    }
+}
