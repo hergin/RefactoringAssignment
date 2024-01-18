a8b49fae2a3443752729f36430330f6b452ced84
jeorozc0 <jeorozco@bsu.edu>
2024-01-17 23:46:02 -0500

    Fine tuning

---
+++ src/InsuranceStrategy.java
@@ -0,0 +1,17 @@
+public class InsuranceStrategy {
+    public static double calculateInsuranceVeryHigh(double income) {
+        return (income - InsuranceStrategy.getAdjustment()) * InsuranceStrategy.getWeight() + InsuranceStrategy.getConstant();
+    }
+
+    public static int getConstant() {
+        return 105600;
+    }
+
+    public static double getWeight() {
+        return 0.02;
+    }
+
+    public static int getAdjustment() {
+        return 60000;
+    }
+}
--- src/InsuranceStrategyVeryHigh.java
+++ src/InsuranceStrategyVeryHigh.java
@@ -1,17 +1,3 @@
-public class InsuranceStrategyVeryHigh {
-    public static double calculateInsuranceVeryHigh(double income) {
-        return (income - getAdjustment()) * getWeight() + getConstant();
-    }
+public class InsuranceStrategyVeryHigh extends InsuranceStrategy {
 
-    public static int getConstant() {
-        return 105600;
-    }
-
-    public static double getWeight() {
-        return 0.02;
-    }
-
-    public static int getAdjustment() {
-        return 60000;
-    }
 }
