ba5fa77b88e394301d53cfa61f9fdc5b72984a34
Justyn Fox <jjfox@bsu.edu>
2024-01-17 21:18:33 -0500

    Using the Extract Delegate refactoring

---
+++ src/InsuranceStrategyVeryHigh.java
@@ -0,0 +1,22 @@
+public class InsuranceStrategyVeryHigh {
+
+    public InsuranceStrategyVeryHigh() {
+
+    }
+
+    static double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    static int getConstant() {
+        return 105600;
+    }
+
+    static double getWeight() {
+        return 0.02;
+    }
+
+    static int getAdjustment() {
+        return 60000;
+    }
+}
