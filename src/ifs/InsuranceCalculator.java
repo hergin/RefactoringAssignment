a392ae83a973de80e934abde2621fa849f4cf720
connerum <98507011+connerum@users.noreply.github.com>
2024-01-17 20:42:10 -0500

    Fine tuning

--- src/ifs/InsuranceCalculator.java
+++ src/ifs/InsuranceCalculator.java
@@ -2,7 +2,8 @@ package ifs;
 
 public class InsuranceCalculator {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+    private InsuranceStrategy strategy;
+
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
@@ -12,7 +13,8 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
     }
 
---
+++ src/ifs/InsuranceStrategy.java
@@ -0,0 +1,13 @@
+package ifs;
+
+public abstract class InsuranceStrategy {
+    double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    abstract int getConstant();
+
+    abstract double getWeight();
+
+    abstract int getAdjustment();
+}
