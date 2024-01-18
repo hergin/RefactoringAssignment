6b708907e8c5e5117ab1e66698c0b532c75726a3
connerum <98507011+connerum@users.noreply.github.com>
2024-01-17 21:05:38 -0500

    Implementing the abstract class

--- src/ifs/InsuranceStrategy.java
+++ src/ifs/InsuranceStrategy.java
@@ -1,7 +1,7 @@
 package ifs;
 
 public abstract class InsuranceStrategy {
-    double calculateInsuranceVeryHigh(double income) {
+    double calculate(double income) {
         return (income - getAdjustment()) * getWeight() + getConstant();
     }
 
