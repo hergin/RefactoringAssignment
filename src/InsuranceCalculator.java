a4b6ca838079ce4662abb6f60255d9c17c7169e0
connorvessely <111907690+connorvessely@users.noreply.github.com>
2024-01-17 20:55:55 -0500

    Bonus completed

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,20 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class InsuranceCalculator {
 
     private InsuranceStrategy strategy;
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
+        strategy = getStrategy(income);
+        return strategy.calculate(income);
+    }
+
+    List<InsuranceStrategy> strategies = Arrays.asList(new InsuranceStrategyLow(),
+            new InsuranceStrategyMedium(), new InsuranceStrategyMedium(),
+            new InsuranceStrategyHigh(), new InsuranceStrategyHigh(), new InsuranceStrategyHigh(),
+            new InsuranceStrategyVeryHigh());
+
+    private InsuranceStrategy getStrategy(double income){
+        double dividing = income/10000.00001;
+        int result = (int) dividing;
+        int index = Math.min(result, 6);
+        return strategies.get(index);
     }
 }
