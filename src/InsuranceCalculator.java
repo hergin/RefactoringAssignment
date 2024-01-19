ac6aaa72a61dddc1e0d65ed01081732b247a1d67
Gavin Neal <grneal@bsu.edu>
2024-01-18 22:29:43 -0500

    Using the Extract Delegate refactoring

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,5 +1,7 @@
 public class InsuranceCalculator {
 
+    private final VeryHighInsuranceStrategy veryHighInsuranceStrategy = new VeryHighInsuranceStrategy();
+
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             return income*0.365;
@@ -8,22 +10,7 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return getVeryHighInsurance(income);
+            return veryHighInsuranceStrategy.getVeryHighInsurance(income);
         }
     }
-
-    private double getVeryHighInsurance(double income) {
-        return (income- getAdjustment())* getWeight() + getConstant();
-    }
-    public int getConstant() {
-        return 105600;
-    }
-
-    public double getWeight() {
-        return 0.02;
-    }
-    public int getAdjustment() {
-        return 60000;
-    }
-
 }
---
+++ src/VeryHighInsuranceStrategy.java
@@ -0,0 +1,20 @@
+public class VeryHighInsuranceStrategy {
+    public VeryHighInsuranceStrategy() {
+    }
+
+    public double getVeryHighInsurance(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getAdjustment() {
+        return 60000;
+    }
+}
