72ab0c4f88c843cd19d99ee036a20aa570d8bbe2
bhhincks <97757988+bhhincks@users.noreply.github.com>
2024-01-19 22:19:56 -0500

    Using the extract delegate refactoring

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,4 +1,5 @@
 public class InsuranceCalculator {
+    private  final InsuranceStrategyVeryHigh insuranceStrategyHighIncome = new InsuranceStrategyVeryHigh();
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
@@ -8,24 +9,8 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return getaDouble(income);
+            return insuranceStrategyHighIncome.calculateInsurance(income);
         }
-    }
-
-    private static double getaDouble(double income) {
-        return (income - getAnInt()) * getaDouble() + getAdjustment();
-    }
-
-    private static int getAdjustment() {
-        return 105600;
-    }
-
-    private static double getaDouble() {
-        return 0.02;
-    }
-
-    private static int getAnInt() {
-        return 60000;
     }
 
 }
